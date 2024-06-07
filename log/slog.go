a591221e7c67d1763b5037329947e3b6d51cedec
cnk3x <me@k3x.cn>
2024-06-07 12:01:30 +0800

    update log

--- log/slog.go
+++ log/slog.go
@@ -172,7 +172,7 @@ func (h *handler) Handle(ctx context.Context, r slog.Record) error {
 	}
 
 	// write source
-	if h.addSource && h.level == slog.LevelDebug {
+	if h.addSource && r.PC > 0 && r.Level == slog.LevelDebug {
 		fs := runtime.CallersFrames([]uintptr{r.PC})
 		f, _ := fs.Next()
 		if f.File != "" {
